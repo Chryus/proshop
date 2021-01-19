f260cc89f739945af1627333ae83ec0ca47ff895
Chris Haack <chris.haack@me.com>
2021-01-19 12:03:55 -0500

    add custom authorization middleware

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,7 +2,7 @@ import asyncHandler from 'express-async-handler';
 import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
 
-// @desc    Auth user and get token
+// @desc    Authenticate user and get token
 // @route   POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
@@ -24,4 +24,23 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// @desc    Authorize user and get profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, getUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userControllers.js';
+import { protect } from '../middleware/authMiddleware.js';
+import { authUser, getUserProfile } from '../controllers/userControllers.js';
 
 router.route('/login').get(authUser);
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;

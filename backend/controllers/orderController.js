d7c6701ee8e82501df136a60c6ebeee1b8c1bcc7
Chris Haack <chris.haack@me.com>
2021-02-11 20:31:13 -0500

    add get order route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,23 @@
 import asyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
 // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
@@ -37,4 +54,4 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems };
+export { addOrderItems, getOrderById };

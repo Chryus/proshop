11097ab45d91c144992d1d7e057700d49c2fd370
Chris Haack <chris.haack@me.com>
2021-02-14 14:45:46 -0500

    add order paid endpoint, reducer/action

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -18,6 +18,30 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Update order to paid
+// @route   PUT /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address
+    };
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
 // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
@@ -54,4 +78,4 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+export { addOrderItems, getOrderById, updateOrderToPaid };
